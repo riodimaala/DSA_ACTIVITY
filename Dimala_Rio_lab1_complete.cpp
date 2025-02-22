d4faa209138c371bf40553ae59569c8314fb70a9
riodimaala <24-18059@g.batstate-u.edu.ph>
2025-02-22 09:55:17 +0800

    activity

---
+++ Dimala_Rio_lab1_complete.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+
+using namespace std;
+
+int myArray[30];
+int numElem = 0;
+int maxElem = 20;
+
+void traverseArray(){
+    cout << "Showing elements in my array \n";
+    for(int i = 0; i< maxElem; i++){
+        cout << to_string(myArray[i]) + " | ";
+    }
+    cout << "\n";
+}
+
+string insertAtTheEnd(int num){
+    if(numElem != maxElem){
+        myArray[numElem] = num;
+        numElem++;
+        return "An element is successfully inserted at the end! \n";
+    }
+
+    return "The Array is full. Please remove elements before inserting new one. \n";
+}
+string inserAtTheBeginning(int num){
+    if(numElem != maxElem){
+        for(int i = numElem; i >= 0; i--){
+            myArray[i+1] = myArray[i];
+            
+        }
+        myArray[0] = num;
+        numElem++;
+        return "An Element is successfully inserted at the beginning! \n";
+    }
+    
+    
+    return "The array is full. please remove elements before inserting new one. \n";
+}
+string insertAtTheGivenPosition(int num, int ElementAfter){
+    if(numElem != maxElem){
+        int position;
+        for(int i = 0; i <= maxElem; i++){
+            if(myArray[i] == ElementAfter){
+                position = i;
+                break;
+            }
+        }
+        
+        for(int i = numElem; i>=position; i--){
+            myArray[i+1] = myArray[i];
+        }
+        myArray[position + 1] = num;
+        numElem++;
+        return "An Element is successfully inserted at the specified location! \n";
+    }
+    
+    return "The array is full. please remove Elements before inserting new one. \n";
+}
+
+
+int main(){
+    
+    insertAtTheEnd(3);
+    insertAtTheEnd(7);
+    insertAtTheEnd(9);
+    inserAtTheBeginning(20);
+    insertAtTheGivenPosition(37,7);
+    inserAtTheBeginning(256);
+    insertAtTheGivenPosition(128,9);
+    inserAtTheBeginning(19);
+    traverseArray();
+    
+    
+    
+    return 0;
+}
